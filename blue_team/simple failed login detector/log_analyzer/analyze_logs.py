8af93600cc5d848f6cdbf040c9ff59fe8a62eaef
saheel <saheelkushwaha5@gmail.com>
2025-07-06 10:58:19 +0545

    Delete blue_team/simple failed login detector directory

--- blue_team/simple failed login detector/log_analyzer/analyze_logs.py
+++
@@ -1,9 +0,0 @@
-count = 0
-with open("sample_log.txt", "r") as file:
-    logs = file.readlines()
-
-for line in logs:
-    if "failed" in line:
-        print("alert", line.strip())
-        count += 1
-print("failed attempts:", count)
